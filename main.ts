f5866b3a32c0fcfcb2dc4629476d4dc2c1d7d2d0
k-okina <okinakahiro@gmail.com>
2019-10-09 10:32:52 +0900

    console.log(1.999 * 2 / 3 + 1 + 1 + "hello world") のastを書いた

--- main.ts
+++ main.ts
@@ -1,5 +1,44 @@
+// https://ts-ast-viewer.com
 import * as ts from 'typescript'
 
-console.log(ts.createStringLiteral("hello"))
+const printer = ts.createPrinter()
 
-console.log(ts.createExpressionStatement(ts.createStringLiteral("hello")))
+function astFactory() {
+  return [
+    ts.createExpressionStatement(ts.createCall(
+      ts.createPropertyAccess(
+        ts.createIdentifier("console"),
+        ts.createIdentifier("log")
+      ),
+      undefined,
+      [ts.createBinary(
+        ts.createBinary(
+          ts.createBinary(
+            ts.createBinary(
+              ts.createBinary(
+                ts.createNumericLiteral("1.999"),
+                ts.createToken(ts.SyntaxKind.AsteriskToken),
+                ts.createNumericLiteral("2")
+              ),
+              ts.createToken(ts.SyntaxKind.SlashToken),
+              ts.createNumericLiteral("3")
+            ),
+            ts.createToken(ts.SyntaxKind.PlusToken),
+            ts.createNumericLiteral("1")
+          ),
+          ts.createToken(ts.SyntaxKind.PlusToken),
+          ts.createNumericLiteral("1")
+        ),
+        ts.createToken(ts.SyntaxKind.PlusToken),
+        ts.createStringLiteral("hello world")
+      )]
+    ))
+  ]
+}
+
+// Result: console.log(1.999 * 2 / 3 + 1 + 1 + "hello world")
+console.log(printer.printList(
+  ts.ListFormat.MultiLine,
+  ts.createNodeArray(astFactory()),
+  ts.createSourceFile("", "", ts.ScriptTarget.ES2015)
+))
